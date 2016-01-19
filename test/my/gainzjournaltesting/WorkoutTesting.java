2436ab5fbe7de4243b6deb26b2a32407ac2bc9b1
Jong Hoon Lim <jhlim84@hotmail.com>
2016-01-19 10:03:46 -0600

    fixed package issues

--- test/my/gainzjournaltesting/WorkoutTesting.java
+++ test/my/gainzjournaltesting/WorkoutTesting.java
@@ -1,3 +1,5 @@
+package my.gainzjournaltesting;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -9,6 +11,8 @@
  * @author jhl2298
  */
 
+import my.gainzjournal.Workout;
+import my.gainzjournal.Exercise;
 import java.util.ArrayList;
 public class WorkoutTesting {
     
